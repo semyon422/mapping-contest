4073e3af10e8a131420d87580e7a6f200032ed7c
semyon422 <semyon422@users.noreply.github.com>
2023-10-20 17:15:11 +0500

    Fix time zones

--- util/datetime.lua
+++ util/datetime.lua
@@ -1,17 +1,18 @@
 local date = require("date")
+local config = require("lapis.config").get()
 
 local Datetime = {}
 
 function Datetime.to_iso(t)
-	return date(t):fmt("${iso}")
+	return date(t + config.timezone * 3600):fmt("${iso}")
 end
 
 function Datetime.to_text(t)
-	return date(t):fmt("%F %T")
+	return date(t + config.timezone * 3600):fmt("%F %T")
 end
 
 function Datetime.to_unix(t)
-	return date.diff(t, "1970-01-01T00:00"):spanseconds()
+	return date.diff(t, "1970-01-01T00:00"):spanseconds() - config.timezone * 3600
 end
 
 return Datetime
