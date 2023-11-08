f64971c20c7c16123d50291bc24120d2c4cb8790
semyon422 <semyon422@users.noreply.github.com>
2023-11-08 15:27:18 +0500

    Use md5 from openssl for filehash

--- util/filehash.lua
+++ util/filehash.lua
@@ -1,25 +1,22 @@
+local digest = require("openssl.digest")
 
 local filehash = {}
 
 function filehash.sum_bin(s)
-	return ngx.md5_bin(s)
+	return digest.new("md5"):final(s)
 end
 
 function filehash.sum(s)
-	return ngx.md5(s)
+	return filehash.decode(filehash.sum_bin(s))
 end
 
 function filehash.encode(hex)
-	if #hex == 16 then
-		return hex
-	end
+	assert(#hex == 32, "invalid length")
     return (hex:gsub("..", function(cc) return string.char(tonumber(cc, 16) or 0) end))
 end
 
 function filehash.decode(data)
-	if #data == 32 then
-		return data
-	end
+	assert(#data == 16, "invalid length")
 	return (data:gsub(".", function(c) return ("%02x"):format(c:byte()) end))
 end
 
