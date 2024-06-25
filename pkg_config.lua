6ce6db5331f2d0b5dd03ed4600d09cf38de29865
semyon422 <semyon422@users.noreply.github.com>
2024-06-25 13:22:04 +0500

    Minor fixes

--- pkg_config.lua
+++ pkg_config.lua
@@ -1,12 +1,16 @@
-local pkg = require("aqua.package")
--- pkg.reset()
-pkg.add(".")
+package.loadlib("/usr/local/opt/sqlite/lib/libsqlite3.dylib","*")
+
+local pkg = require("aqua.pkg")
+
 pkg.add("aqua")
 pkg.add("tree/share/lua/5.1")
 pkg.addc("tree/lib/lua/5.1")
--- pkg.add(os.getenv("LJ_ROOT") .. "/share/luajit-2.1.0-beta3")
--- pkg.add(os.getenv("OR_ROOT") .. "/lualib")
--- pkg.addc(os.getenv("OR_ROOT") .. "/lualib")
+
+pkg.add(os.getenv("LJ_ROOT") .. "/share/luajit-2.1.0-beta3")
+pkg.add(os.getenv("OR_ROOT") .. "/lualib")
+pkg.addc(os.getenv("OR_ROOT") .. "/lualib")
 
 pkg.add("glue")
 pkg.add("minizip")
+
+pkg.export_lua()
