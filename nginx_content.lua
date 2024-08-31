e2243d934473d88db76297073d5f5e383961ba61
semyon422 <semyon422@users.noreply.github.com>
2024-08-31 21:46:58 +0500

    Update aqua

--- nginx_content.lua
+++ nginx_content.lua
@@ -1,6 +1,8 @@
 package.loaded.utf8 = require("lua-utf8")
 
 local etlua_util = require("http.etlua_util")
+local NginxRequest = require("web.nginx.NginxRequest")
+local NginxResponse = require("web.nginx.NginxResponse")
 local App = require("app.App")
 
 local app = App()
@@ -9,27 +11,7 @@ app:load()
 table.insert(package.loaders, etlua_util.loader)
 
 return function()
-	local req = {}
-
-	req.headers = ngx.req.get_headers()
-	req.method = ngx.req.get_method()
-	req.uri = ngx.var.request_uri
-
-	local ok, code, headers, body = app.webApp:handle(req)
-	if not ok then
-		ngx.status = 500
-		ngx.print("<pre>" .. tostring(code) .. "</pre>")
-		return
-	end
-
-	if not code then
-		ngx.status = 404
-		return
-	end
-
-	ngx.status = code
-	for k, v in pairs(headers) do
-		ngx.header[k] = v
-	end
-	ngx.print(body)
+	local req = NginxRequest()
+	local res = NginxResponse()
+	app.webApp:handle(req, res)
 end
